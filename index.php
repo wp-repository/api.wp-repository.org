584da89a0a1b0164d692cefd96ce9ff5eb29daf4
Christian Foellmann <foellmann@foe-services.de>
2013-06-21 12:36:41 +0200

    comment about POST

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Accept json from POST if keys match
+ * 
+ * private API key = 16 alphanumeric
+ * 
+ * if successful -> 
+ *		- create mirror
+ *		- svn2git
+ *		- add cronjob
  */
 ?>
